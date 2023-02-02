edf883434b17b9c1e4c1453c8164f04accbcb947
Kamil <kamildziubak19@gmail.com>
2023-02-02 14:03:20 +0100

    First commit

---
+++ src/test/java/pl/edu/vistula/Cwiczenia15/Cwiczenia15ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Cwiczenia15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cwiczenia15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
